a2cd46073b53c9da46da39a847f6b3cfaa7b3d46
ramseskian <90127672+ramseskian@users.noreply.github.com>
2025-12-07 19:02:37 +0800

    Update spaceinvaderstest.c

--- spaceinvaderstest.c
+++ spaceinvaderstest.c
@@ -246,7 +246,6 @@ void update_game() { //function to update game state
 
 void clear_screen() {
     system("cls");  //clears screen (so that you do not need to scroll up to see the updated board)
-    system("clear");
 }
 
 //function to read high score from file
@@ -269,4 +268,5 @@ void save_high_score() {
         fclose(file);                         //close file after writing
     }
 }
+
 
